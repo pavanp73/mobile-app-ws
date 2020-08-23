a1e8c7fb5655f673439535c20c52364f33d1a21d
Pavan Padmanabhi <pavan.padmanabhi@gmail.com>
2020-08-23 16:58:45 +0530

    Add User controller

---
+++ src/main/java/com/pavan/apps/ws/restcontroller/UserController.java
@@ -0,0 +1,15 @@
+package com.pavan.apps.ws.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+	@GetMapping
+	public String getUsers() {
+		return "get users method";
+	}	
+}
