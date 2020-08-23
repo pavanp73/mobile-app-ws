241517f4d74fec1235d31ace8d537d5e4fcb9460
Pavan Padmanabhi <pavan.padmanabhi@gmail.com>
2020-08-23 21:54:41 +0530

    Adding UserDto class and modifying UserController class

--- src/main/java/com/pavan/apps/ws/restcontroller/UserController.java
+++ src/main/java/com/pavan/apps/ws/restcontroller/UserController.java
@@ -1,8 +1,14 @@
 package com.pavan.apps.ws.restcontroller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pavan.apps.ws.models.UserDto;
 
 @RestController
 @RequestMapping("users")
@@ -11,5 +17,25 @@ public class UserController {
 	@GetMapping
 	public String getUsers() {
 		return "get users method";
-	}	
+	}
+	
+	@GetMapping("/{userId}")
+	public UserDto getUser(@PathVariable("userId") long userId) {
+		return new UserDto();
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "create user";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user";
+	}
 }
